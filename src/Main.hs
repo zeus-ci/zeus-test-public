53d1d05566bd0619c36ccce618260cbd7318f2cc
Doug Beardsley <mightybyte@gmail.com>
2019-04-07 15:44:18 -0400

    Back to basics

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,3 @@ module Main where
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
-  putStrLn "Another line"
-  putStrLn "Another line"
-  putStrLn "Another line"
-  putStrLn "Another line"
-  putStrLn "Created a test branch"
-  putStrLn "tb2"
-  putStrLn "tb2 update"
-  putStrLn "New line"
