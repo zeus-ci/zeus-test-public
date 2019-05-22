3f676b681638506abcad862c921e3fb288be45aa
Doug Beardsley <mightybyte@gmail.com>
2019-05-22 12:25:54 -0400

    Fix errors

--- src/Main.hs
+++ src/Main.hs
@@ -6,8 +6,8 @@ main :: IO ()
 main = do
   putStrLn "Hello out there!"
   putStrLn "Another line"
-  putStrn "Yet another line"
-  putStrn "line"
-  putStrn "line"
-  putStrn "line"
-  putStrn "line"
+  putStrLn "Yet another line"
+  putStrLn "line"
+  putStrLn "line"
+  putStrLn "line"
+  putStrLn "line"
