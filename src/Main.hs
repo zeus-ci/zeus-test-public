c5152e36fb9abf5247c2dd64d0659eb11dfdc480
Doug Beardsley <mightybyte@gmail.com>
2019-06-02 03:00:35 -0400

    Restart

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,3 @@ module Main where
 main :: IO ()
 main = do
   putStrLn "Hello out there!"
-  putStrLn "Another line"
-  putStrLn "Yet another line"
-  putStrLn "line"
-  putStrLn "line"
-  putStrLn "line"
-  putStrLn "line"
-  putStrLn "line"
-  putStrLn "line"
